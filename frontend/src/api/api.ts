b5ce3bba3fc9e5feecde518f329d0632106d31f5
condricnay <naytitorn21@gmail.com>
2026-03-24 19:13:22 +0700

    added vercel.json and bypass ngrok warning

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,8 +1,16 @@
 import type { SessionFormSchema } from "./types";
 import { BACKEND_URL } from "@/config";
 
-export const apiFetch = async (path: string, options: RequestInit) => {
-  const res = await fetch(`${BACKEND_URL}${path}`, options);
+export const apiFetch = async (path: string, options: RequestInit = {}) => {
+  const headers = {
+    "ngrok-skip-browser-warning": "true",
+    ...options.headers,
+  };
+
+  const res = await fetch(`${BACKEND_URL}${path}`, { 
+    ...options, 
+    headers 
+  });
 
   if (!res.ok) {
     throw new Error(await res.text());
@@ -15,50 +23,37 @@ export async function getPlotUrl(session: SessionFormSchema, view: string, runId
   const res = await fetch(`${BACKEND_URL}/plot?view=${view}&runId=${runId}`, {
     method: "POST",
     headers: {
-      "Content-Type": "application/json"
+      "Content-Type": "application/json",
+      "ngrok-skip-browser-warning": "true" 
     },
     body: JSON.stringify(session)
-  })
+  });
 
   if (!res.ok) {
-    throw new Error("Plot request failed")
+    throw new Error("Plot request failed");
   }
 
-  const blob = await res.blob()
-  return URL.createObjectURL(blob)
+  const blob = await res.blob();
+  return URL.createObjectURL(blob);
 }
 
-
 export async function getSubjects(): Promise<string[]> {
-  const res = await fetch(`${BACKEND_URL}/participants/`)
-  if (!res.ok) throw new Error("Failed to fetch subjects")
-  return await res.json()
+  return apiFetch(`/participants/`);
 }
 
 export async function getTasks(subject: string) {
-  const res = await fetch(`${BACKEND_URL}/participants/${subject}/tasks/`)
-  if (!res.ok) throw new Error("Failed to fetch tasks")
-  const data = await res.json()
-  return data.tasks as [string, string | null][]
+  const data = await apiFetch(`/participants/${subject}/tasks/`);
+  return data.tasks as [string, string | null][];
 }
 
 export async function fetchTableData(
   session: SessionFormSchema, view: string, runId: string
 ) {
-  const res = await fetch(
-    `${BACKEND_URL}/data?view=${view}&runId=${runId}`,
-    {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(session),
-    }
-  )
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch table data")
-  }
-
-  return res.json()
+  return apiFetch(`/data?view=${view}&runId=${runId}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(session),
+  });
 }
